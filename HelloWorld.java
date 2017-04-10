2c045743655dfb9d4643c66b67da640bf178c39d
Franklin Smith <f2vs2@hotmail.com>
2017-04-10 11:24:48 -0700

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,26 @@
+// need to make a class for every file
+// everything in Java is a class
+// have a main function but wrapped up in a class
+// name of class be the same exact name as name of the file
+// then we need a main function
+// main function is the entry point into the program
+// operating system is calling your program similar as to how you would call a function
+// when the OS runs program it dosent know where to begin unless you tell it and you tell it 
+// by giving it a main function
+// highest level function in program its the funciton thats ran first
+// in main function you can call other funcitons
+// to compile you type javac filename.java
+// to run you type java helloworld
+// add java compiler to my path
+// when you type javac you are envoking java compiler
+// command execution -> javac HelloWorld.java
+// java HelloWorld
+public class HelloWorld{
+
+	public static void main(String[] args){
+		System.out.println("Hello World!");
+
+	}
+
+
+}
