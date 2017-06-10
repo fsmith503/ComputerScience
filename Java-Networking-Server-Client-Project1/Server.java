7f15f5dcc94dc1b0ca945376eea28b3bb99a2fbc
Franklin Smith <f2vs2@hotmail.com>
2017-06-09 20:45:26 -0700

    Create Server.java

--- Java-Networking-Server-Client-Project1/Server.java
+++ Java-Networking-Server-Client-Project1/Server.java
@@ -5,6 +5,27 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 //when testing always run server first
+/*
+The goal of this assignment was for me to gain experience with network programming, 
+specifically with sockets in Java. This assignment will also required me to 
+develop a server and a client simultaneously. The client will allow the user
+to enter an arbitrary list of integers to be sent to the server. The server 
+will then return a list of prime integers contained in the original list. 
+
+I Created two classes, one for my client and one for my server. 
+Each class will need a main method so that both classes can be executed. 
+I Implemented my client to prompt the user to enter a list of integers 
+(and use "!" to end the list.) Send the list of integers to the server 
+(hint: see java.net.Socket), print the list of integers sent to the server,
+and later print the list of prime integers received from the server. 
+
+I Implemented my server to wait for a socket connection
+When a socket is accepted, read a list 
+of integers, determine which in the list are prime, and then send a list 
+containing the prime integers back to my client. 
+*/
+
+
 public class Server {
 	private static final int PORT = 1335;
 	static boolean isPrime(int num) {
