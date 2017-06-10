b5bd864a05f7625e854d597df8e9c9115702545c
Franklin Smith <f2vs2@hotmail.com>
2017-06-09 21:02:01 -0700

    Create Main.java

--- Java-Networking-Server-Client-Project2/Main.java
+++ Java-Networking-Server-Client-Project2/Main.java
@@ -2,6 +2,20 @@ import java.util.Scanner;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+/*
+I wrote a client which generates a list of 5 integers randomly in the
+range [2,100), prints them, sends them to the server, and receives and 
+prints the list of prime integers contained in the original list. My client
+sleeps for 1 second everytime after it revieves and prints the prime integers.
+My client keeps repeating this procedure until stopped by inputting "!". In 
+order to keep my server responsive, the server doesnt shut down after it 
+sends the prime integers back to the client.
+ Use "!" to stop and start sending integers to the server, and use "#" to quit 
+ the client. My client was able to read inputs while sending or recieving data.
+ when you input "#" the client shuts down and quits
+
+*/
+
 public class Main {
 	static String num_string;
 	public static boolean sending = false;
