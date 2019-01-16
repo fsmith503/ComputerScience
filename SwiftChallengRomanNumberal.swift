89441483253b19e49909d15ab1ae67e1a8332ca1
Franklin Smith <f2vs2@hotmail.com>
2019-01-16 12:06:22 -0800

    Update SwiftChallengRomanNumberal.swift

--- SwiftChallengRomanNumberal.swift
+++ SwiftChallengRomanNumberal.swift
@@ -1,3 +1,5 @@
+/*
+
 Create a function taking a positive integer as its parameter and returning a string containing the Roman Numeral representation of that integer.
 
 Modern Roman numerals are written by expressing each digit separately starting with the left most digit and skipping any digit with a value of zero. In Roman numerals 1990 is rendered: 1000=M, 900=CM, 90=XC; resulting in MCMXC. 2008 is written as 2000=MM, 8=VIII; or MMVIII. 1666 uses each Roman symbol in descending order: MDCLXVI.
@@ -16,6 +18,9 @@ C          100
 D          500
 M          1,000
 Remember that there can't be more than 3 identical symbols in a row.
+
+
+*/
 
 
 
